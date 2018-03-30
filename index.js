0ed128ef0a79454c44a3f7e5f06927e8f42216fe
Alexander Urizar <urizaraj@gmail.com>
2018-03-30 20:23:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ function getRepositories() {
 }
 
 function showRepositories(event, data) {
-  //this is set to the XMLHttpRequest object that fired the event
-  console.log(this.responseText)
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
 }
