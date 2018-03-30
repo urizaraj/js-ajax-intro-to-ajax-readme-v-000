717995eaec20a1d9eec50c7fb24e4465a1af6d12
Alexander Urizar <urizaraj@gmail.com>
2018-03-30 20:22:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function getRepositories() {
   req.open("GET", 'https://api.github.com/users/octocat/repos')
   req.send()
 }
+
+function showRepositories(event, data) {
+  //this is set to the XMLHttpRequest object that fired the event
+  console.log(this.responseText)
+}
